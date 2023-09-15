5b9c44c24983aac68369d5ab81c36f2be3419c66
Weaver <95450946+Enjoyweaver@users.noreply.github.com>
2023-09-14 22:06:59 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/banner1.png"
           alt="Next.js Logo"
-          width={400}
-          height={300}
+          width={600}
+          height={400}
           priority
         />
       </div>
