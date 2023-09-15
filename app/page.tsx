9764f73d416199dbf35ad608c7e214d5f4f4f5e6
Weaver <95450946+Enjoyweaver@users.noreply.github.com>
2023-09-15 01:21:40 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,19 +22,20 @@ export default function Home() {
       <div className="custom-text" id="targetText">...of the utmost good faith</div>
 
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          style={{ color: 'red' }}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold default-color`}>
-            Docs{' '}
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Policy Coverage{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Bitcoin Captive Insurance.
+            Learn about the policy coverage.
           </p>
         </a>
 
