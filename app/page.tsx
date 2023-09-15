ee186ac844a76038e34a1435ebb0e5a98e8ecb62
Weaver <95450946+Enjoyweaver@users.noreply.github.com>
2023-09-15 01:17:56 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="custom-text" id="targetText">...of the utmost, and most abundant, good faith</div>
+      <div className="custom-text" id="targetText">...of the utmost good faith</div>
 
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 my-4 default-color"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
